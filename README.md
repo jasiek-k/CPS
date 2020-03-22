c0a7cfb3cda3b4fd6cf07b3fa3f13a630f5987fe
216797 Klamka Jan <216797@edu.p.lodz.pl>
2020-03-22 02:43:38 +0100

    signal operations added

--- README.md
+++ README.md
@@ -2,14 +2,14 @@ Uruchamianie programu:
 >>> python main.py
 
 W zależności od wybranego wariantu należy sprecyzować parametry wywołania:
-[S1] [A] [t1] [d] [bins] [f] ['T']
+[S1/S2] [A] [t1] [d] [bins] [f] ['T']
 [S3/S4/S5] [A] [T] [t1] [d] [bins] [f] ['T']
-[S6/S7/S8] [A] [T] [t1] [d] [kw] [bins] [f] ['T]
+[S6/S7/S8] [A] [T] [t1] [d] [kw] [bins] [f] ['T']
 [S9] [A] [t1] [d] [ts] [bins] [f] ['T']
 [S10] [A] [n1] [ns] [l] [bins] [f] ['T']
-[S11]
+[S11] [A] [t1] [d] [f] [p] [bins] 
 ['R'] ['nazwa_pliku'] 
-[parametry1] [dodaj/odejmij/mnoz/dziel] [parametry2]
+[parametry1] [dodaj/odejmij/mnoz/dziel] [parametry2] [f] ['Z']
 
 Gdzie:
 S1 - szum o rozkładzie jednostajnym
@@ -27,14 +27,17 @@ S11 - szum impulsowy
 bins - liczba przedziałów histogramu 
 'T' - zapisz wyniku operacji do pliku (należy opuścić jeśli wynik ma nie zostać zapisany)
 'R' - operuj na wartościach wczytanych z pliku binarnego
+'Z' - zapisz wyniku operacji do pliku - flaga dotyczy jedynie wyniku operacji na 2 sygnałach!
 
 Przykładowe wywołanie programu:
 
 Struktura projektu 
-> sin_signal - sygnał sin, sin wyprostowany jednopołówkowo i dwupołówkowo
-> impuls_signal - impuls jednostkowy, szum impulsowy
-> prost_signal - sygnał prostokątny i prostokątny symetryczny
-> skok_signal - skok jednostkowy
-> troj_signal - sygnał trójkątny
-> impuls_ops - zbiór funkcji przeznaczonych dla przetwarzania impulsów
-> signal_ops - zbiór funkcji przeznaczonych dla przetwarzania sygnałów
+./src/
+    > sin_signal - sygnał sin, sin wyprostowany jednopołówkowo i dwupołówkowo
+    > impuls_signal - impuls jednostkowy, szum impulsowy
+    > prost_signal - sygnał prostokątny i prostokątny symetryczny
+    > skok_signal - skok jednostkowy
+    > troj_signal - sygnał trójkątny
+    > impuls_ops - zbiór funkcji przeznaczonych dla przetwarzania impulsów
+    > signal_ops - zbiór funkcji przeznaczonych dla przetwarzania sygnałów
+./files/
--- src/dzialania.py
+++ src/dzialania.py
@@ -1,19 +1,154 @@
+from matplotlib import pyplot as plt 
+import numpy as np
 
 
 class Dzialania:
 
-    def __init__(self, obj1, obj2):
+    def __init__(self, obj1, obj2, f, string_copy):
         self.obj1 = obj1
         self.obj2 = obj2
+        self.string_copy = string_copy
+        self.f = f
+        self.n1 = 0
+        self.n2 = 0
+        self.przedzial1 = []
+        self.przedzial2 = []
+        self.x_values = []
+        self.y_values = []
+        self.ustaw_przedzial()
+
+    def plik_zapisz(self, nazwa_pliku):
+        plik = open(f'{nazwa_pliku}', 'w')
+        plik.write(f'{self.string_copy} \n') 
+        plik.write(f'{self.y_values}')    
+        plik.close()
+
+    def print(self):
+        print(self.n1)
+        print(self.n2)
+        print(f'{self.przedzial1[0]} {self.przedzial1[1]}')
+        print(f'{self.przedzial2[0]} {self.przedzial2[1]}')
+
+    def ustaw_przedzial(self):
+        if self.obj1.obj.signal == "S10":
+            start1 = self.obj1.obj.n1
+            end1 = self.obj1.obj.n1 + self.obj1.obj.l
+            self.przedzial1 = [start1, end1]
+        else:
+            start1 = self.obj1.obj.t1
+            end1 = self.obj1.obj.t1 + self.obj1.obj.d
+            self.przedzial1 = [start1, end1]
+
+        if self.obj2.obj.signal == "S10":
+            start2 = self.obj2.obj.n1
+            end2 = self.obj2.obj.n1 + self.obj2.obj.l
+            self.przedzial2 = [start2, end2]
+
+        else:
+            start2 = self.obj2.obj.t1
+            end2 = self.obj2.obj.t1 + self.obj2.obj.d
+            self.przedzial2 = [start2, end2] 
+
+        if start1 < start2:
+            self.n1 = start1
+        else:
+            self.n1 = start2
+
+        if end1 < end2:
+            self.n2 = end2
+        else:
+            self.n2 = end1
 
     def dodaj(self):
-        print("+")
+        x_values = np.arange(self.n1, self.n2, (1 / self.f))
+        y_values = []
+
+        for i in np.arange(len(x_values)):
+            if self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and self.przedzial2[0] <= x_values[i] <= self.przedzial2[1]:
+                value = self.obj1.obj.signal(x_values[i]) + self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+            elif self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and (self.przedzial2[0] > x_values[i] or x_values[i] > self.przedzial2[1]):
+                value = self.obj1.obj.signal(x_values[i]) 
+                y_values.append(value)
+            else:
+                value = self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+
+        self.x_values = x_values
+        self.y_values = y_values
+        print(x_values)
+        print(y_values)
+
 
     def odejmij(self):
-        print("-")
+        x_values = np.arange(self.n1, self.n2, (1 / self.f))
+        y_values = []
+
+        for i in np.arange(len(x_values)):
+            if self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and self.przedzial2[0] <= x_values[i] <= self.przedzial2[1]:
+                value = self.obj1.obj.signal(x_values[i]) - self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+            elif self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and (self.przedzial2[0] > x_values[i] or x_values[i] > self.przedzial2[1]):
+                value = self.obj1.obj.signal(x_values[i]) 
+                y_values.append(value)
+            else:
+                value = self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+
+        self.x_values = x_values
+        self.y_values = y_values
 
     def mnoz(self):
-        print("*")
+        x_values = np.arange(self.n1, self.n2, (1 / self.f))
+        y_values = []
+
+        for i in np.arange(len(x_values)):
+            if self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and self.przedzial2[0] <= x_values[i] <= self.przedzial2[1]:
+                value = self.obj1.obj.signal(x_values[i]) * self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+            elif self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and (self.przedzial2[0] > x_values[i] or x_values[i] > self.przedzial2[1]):
+                value = self.obj1.obj.signal(x_values[i]) 
+                y_values.append(value)
+            else:
+                value = self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+
+        self.x_values = x_values
+        self.y_values = y_values
 
     def dziel(self):
-        print("/")
+        x_values = np.arange(self.n1, self.n2, (1 / self.f))
+        y_values = []
+
+        for i in np.arange(len(x_values)):
+            if self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and self.przedzial2[0] <= x_values[i] <= self.przedzial2[1]:
+                if self.obj2.obj.signal(x_values[i]) == 0:
+                    div = 1
+                else: 
+                    div = self.obj2.obj.signal(x_values[i])
+                value = self.obj1.obj.signal(x_values[i]) / div
+                y_values.append(value)
+            elif self.przedzial1[0] <= x_values[i] <= self.przedzial1[1] and (self.przedzial2[0] > x_values[i] or x_values[i] > self.przedzial2[1]):
+                value = self.obj1.obj.signal(x_values[i]) 
+                y_values.append(value)
+            else:
+                value = self.obj2.obj.signal(x_values[i])
+                y_values.append(value)
+
+        self.x_values = x_values
+        self.y_values = y_values
+
+    def wykres(self):
+        plt.title("Wykres zależności amplitudy od czasu") 
+        plt.xlabel("x axis caption") 
+        plt.ylabel("y axis caption")
+        plt.plot(self.x_values, self.y_values) 
+        plt.grid(True)
+        """
+        plt.title("Wykres zależności amplitudy od czasu") 
+        plt.xlabel("x axis caption") 
+        plt.ylabel("y axis caption")
+        plt.hist(self.values, self.bins, facecolor='blue', alpha=0.5)
+        plt.grid(True)
+        """
+        plt.show() 
